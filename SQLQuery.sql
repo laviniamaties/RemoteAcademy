d885c969c555629da61b033888848507a26f41ae
laviniamaties <37396893+laviniamaties@users.noreply.github.com>
2020-02-23 11:12:26 +0200

    Update SQLQuery.sql

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -18,17 +18,19 @@ SELECT *
 FROM Donors
 
 SELECT * 
-FROM Colection_Points
+FROM CollectionPoints
 
 SELECT * 
-FROM Campains
-WHERE Name = 'Campain1'
+FROM Campaigns
+WHERE Name = 'Campaign1'
 
-SELECT d.DonorID, p.Colection_PointID,  d.FirstName, d.LastName
+SELECT d.DonorID, p.ColectionPointID,  d.FirstName, d.LastName, c.Name
 FROM Donors d
-INNER JOIN Colection_Points p ON d.DonorID = p.DonorID
-INNER JOIN Campains c ON c.Colection_PointID = p.Colection_PointID
-WHERE c.Name = 'Campain1'
+INNER JOIN Registers r ON r.DonorID = d.DonorID
+INNER JOIN CollectionPoints p ON p.CollectionPointID = r.CollectionPointID
+INNER JOIN RegisterCampaigns rc ON rc.CollectionPointID = p.CollectionPointID
+INNER JOIN Campaigns c ON c.CampaignID = rc.CampaignID
+WHERE c.Name = 'Campaign1'
 
 --3. Numarul donatorilor pacienti
 
@@ -37,96 +39,108 @@ FROM Donors
 WHERE Type LIKE 'Pac%'
 
 --4. Numarul campaniilor efectuate de banca X
-SELECT COUNT(c.CampainID) 
-FROM Campains c
-INNER JOIN Colection_Points p ON c.Colection_PointID = p.Colection_PointID
-INNER JOIN Blood_Bank b ON b.Colection_PointID = c.Colection_PointID
-WHERE b.Name = 'Bank1'
+SELECT COUNT(c.CampaignID) 
+FROM Campaigns c
+INNER JOIN RegisterCampaigns rc ON rc.CampaignID = c.CampaignID
+INNER JOIN CollectionPoints p ON p.CollectionPointID = rc.CollectionPointID
+INNER JOIN BloodBanks b ON b.BloodBankID = p.BloodBankID
+WHERE b.Name = 'Bank3'
 
 SELECT * 
-FROM Blood_Bank
-WHERE Name = 'Bank1'
+FROM BloodBanks
+WHERE Name = 'Bank3'
 
 SELECT * 
-FROM Campains
+FROM Campaigns
 
---5. Donatorii cu grupa de sange BI pozitiv care au donat la un punct de colectare din Mures
+--5. Donatorii cu grupa de sange B pozitiv care au donat la un punct de colectare din Mures
 
-SELECT d.FirstName, d.LastName, d.Type, t.Description, p.City
+SELECT d.FirstName, d.LastName, d.Type, t.BloodType, p.City
 FROM Donors d
-INNER JOIN Blood_Type t ON t.DonorID = d.DonorID
-INNER JOIN Colection_Points p ON p.DonorID = d.DonorID
-WHERE d.Type = 'Donor' AND t.Description  LIKE 'BI P%' AND p.City = 'Mures'
+INNER JOIN BloodTypes t ON t.BloodTypeID = d.BloodTypeID
+INNER JOIN Registers r ON r.DonorID = d.DonorID
+INNER JOIN CollectionPoints p ON p.CollectionPointID = r.CollectionPointID
+WHERE d.Type = 'Donor' AND t.BloodType  LIKE 'B P%' AND p.City = 'Mures'
+ORDER BY d.FirstName, d.LastName,p.City 
 
 SELECT * 
-FROM Blood_Type
-WHERE Description LIKE 'BI P%'
-
-SELECT * 
-FROM Colection_Points
-WHERE City = 'Mures'
-ORDER BY Name, Address
+FROM BloodTypes
+WHERE BloodType LIKE 'B P%'
 
 
 --6. Numarul bancilor care au obtinut rezerve in urma campaniilor din ultimii 2 ani 
 SELECT *
-FROM Campains
-WHERE Input_Date >= '2018-01-01'
+FROM Campaigns
+WHERE StartDate >= '2018-01-01'
+SELECT *
+FROM BloodBanks
 
-SELECT  COUNT(b.Blood_BankID), b.Name AS Bank_Name, c.Name AS Companie_Name
-FROM Blood_Bank b
-INNER JOIN Colection_Points p ON b.Colection_PointID = p.Colection_PointID
-INNER JOIN Campains c ON c.Colection_PointID = p.Colection_PointID
-WHERE c.Input_Date >= '2018-01-01'
-GROUP BY b.Name,  c.Name 
+SELECT  COUNT(b.BloodBankID), b.Name AS Bank_Name, c.Name AS Companie_Name, c.StartDate
+FROM BloodBanks b
+INNER JOIN CollectionPoints p ON b.BloodBankID = p.BloodBankID
+INNER JOIN RegisterCampaigns rc ON p.CollectionPointID = rc.CollectionPointID
+INNER JOIN Campaigns c ON c.CampaignID = rc.CampaignID
+WHERE c.StartDate >= '2018-01-01'
+GROUP BY b.Name,  c.Name , c.StartDate
 
--- 7. La cate campanii a participat donatorul cu grupa de sange BIII ?
+-- 7. La cate campanii a participat donatorul cu grupa de sange A ?
 
 SELECT *
 FROM Donors
 
 
 SELECT
---COUNT(c.CampainID)
- d.FirstName, d.LastName, t.Description, c.Description AS Campain
-FROM Campains c
-INNER JOIN Colection_Points p ON p.Colection_PointID = c.Colection_PointID
-INNER JOIN Donors d ON d.DonorID = p.DonorID
-INNER JOIN Blood_Type t ON t.DonorID = d.DonorID 
-WHERE t.Description LIKE 'BIII %' AND d.Type = 'Donor'
+COUNT(DISTINCT c.CampaignID)
+-- d.FirstName, d.LastName, t.BloodType, c.Name AS Campaign
+FROM Campaigns c
+INNER JOIN RegisterCampaigns rc ON  c.CampaignID = rc.CampaignID
+INNER JOIN CollectionPoints p ON p.CollectionPointID = rc.CollectionPointID
+INNER JOIN Registers r ON p.CollectionPointID = r.CollectionPointID
+INNER JOIN Donors d ON d.DonorID = r.DonorID
+INNER JOIN BloodTypes t ON t.BloodTypeID = d.BloodTypeID 
+WHERE t.BloodType LIKE 'B %' AND d.Type = 'Donor'
 
 -- 8. Numarul comenzilor efectuate de banca X 
 
 SELECT * 
 FROM Orders
 SELECT * 
-FROM Blood_Bank
+FROM BloodBanks
 
 SELECT COUNT(o.OrderID)
 -- o.Description
 FROM Orders o 
-INNER JOIN Blood_Bank b ON B.Blood_BankID = o.Blood_BankID
-WHERE b.Name = 'Bank3'
+INNER JOIN BloodBanks b ON b.BloodBankID = O.BloodBankID
+WHERE b.Name = 'Bank1'
 
 
--- 9. Lista donatorilor cu  grupa BI
-SELECT * 
-FROM Donors 
-WHERE Type = 'Pacient'
+-- 9. Lista donatorilor cu  grupa AB
 
 SELECT *  
-FROM Blood_Type 
-WHERE Description LIKE 'BI %'
+FROM BloodTypes 
+WHERE BloodType LIKE 'AB %'
 
-SELECT d.FirstName, t.Description, d.Type
+SELECT d.FirstName, t.BloodType, d.Type
 FROM Donors d
-INNER JOIN Blood_Type t ON t.DonorID = d.DonorID
-WHERE t.Description LIKE 'BI %' AND d.Type = 'Donor'
+INNER JOIN BloodTypes t ON t.BloodTypeID = d.BloodTypeID
+WHERE t.BloodType LIKE 'AB %' AND d.Type = 'Donor'
 
--- 10. Numarul punctelor de colectare din fiecare tara
 
-SELECT count(Colection_PointID), Country
-FROM Colection_Points
+SELECT TOP 3 Name
+FROM CollectionPoints
+
+select distinct name 
+from ColectionPoints
+
+Select distinct FirstName
+from Donors
+
+SELECT TOP 50 PERCENT * FROM Donors;
+
+-- 10. Numarul punctelor de colectare din fiecare tara in care avem cel putin 2 puncte de colectare
+
+SELECT count(DISTINCT CollectionPointID), Country
+FROM CollectionPoints
 GROUP BY Country
-HAVING count(Colection_PointID) > 2
+HAVING count(DISTINCT CollectionPointID) >= 2
 ORDER BY Country
